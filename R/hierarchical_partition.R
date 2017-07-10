a3c5366db0f368eb4c13dfd4e0088eb2f456e728
Zuguang Gu <jokergoo@gmail.com>
2017-07-10 09:25:12 +0200

    backup

--- R/hierarchical_partition.R
+++ R/hierarchical_partition.R
@@ -2,27 +2,39 @@
 HierarchicalPartition = setClass("HierarchicalPartition",
     slots = list(
         list = "list",
-        hierarchy = "matrix"
+        hierarchy = "matrix",
+        subgroup = "character"
     )
 )
 
-hierarchical_partition = function(data, column_index = seq_len(ncol(data)), reduce = 0.5, PAC_cutoff = 0.2, min_samples = 6, ...) {
+hierarchical_partition = function(data, column_index = seq_len(ncol(data)), PAC_cutoff = 0.2, min_samples = 6, ...) {
 	
 	.h_obj = new.env()
 	.h_obj$hierarchy = matrix(nrow = 0, ncol = 2)
 	.h_obj$list = list()
+	.h_obj$subgroup = rep("", length(column_index))
 
-	.hierarchical_partition = function(data, column_index = seq_len(ncol(data)), reduce = 0.5, PAC_cutoff = 0.2, 
+	.hierarchical_partition = function(data, column_index = seq_len(ncol(data)), PAC_cutoff = 0.2, 
 		node_id = '0', min_samples = 6, ...) {
 
 		qqcat("\nsubmatrix with @{length(column_index)} columns, node_id: @{node_id}.\n")
 	    part = consensus_partition(data, column_index = column_index, ...)
 
 	    .h_obj$list[[node_id]] = part
+	    .h_obj$subgroup[column_index] = node_id
 
 	    k = get_best_k(part)
 	    cl = get_class(part, k = k)
-	    
+
+		oe = try(sig <- get_signatures(part, k = k, plot = FALSE))
+		if(inherits(oe, "try-error")) {
+			return(NULL)
+		}
+		if(length(sig$group) < 100) {
+			qqcat("Number of signatures are too small (< 100), stop.\n")
+			return(NULL)
+		}
+
 	    mat = part@.env$data[, part@.env$column_index, drop = FALSE]
 	    mat = t(scale(t(mat)))
 	    mat = mat[order(part@.env$all_value_list[[1]], decreasing = TRUE)[1:max(part@top_n)], , drop = FALSE]
@@ -67,12 +79,14 @@ hierarchical_partition = function(data, column_index = seq_len(ncol(data)), redu
 	    return(NULL)
 	}
 
-	.hierarchical_partition(data, column_index = column_index, reduce = reduce, PAC_cutoff = PAC_cutoff, 
+	.hierarchical_partition(data, column_index = column_index, PAC_cutoff = PAC_cutoff, 
 		min_samples = min_samples, node_id = "0", ...)
 
 	hp = new("HierarchicalPartition")
 	hp@hierarchy = .h_obj$hierarchy
 	hp@list = .h_obj$list
+	hp@subgroup = .h_obj$subgroup
+	names(hp@subgroup) = colnames(data)[column_index]
 
 	return(hp)
 }
