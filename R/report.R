a6eeb6a15b434d8c0debc74f18933edab113f56f
Zuguang Gu <jokergoo@gmail.com>
2017-09-21 00:18:47 +0200

    generate Rmd based on templates

--- R/report.R
+++ R/report.R
@@ -1,1 +1,10 @@
- 
+cola_report = function(object, output) {
+	txt = readLines("~/project/cola/inst/extdata/cola_report_template.Rmd")
+	txt = paste(txt, collapse = "\n")
+	txt = qq(txt, code.pattern = "\\[%CODE%\\]")
+
+	tempfile = tempfile(tmpdir = getwd(), fileext = ".Rmd")
+	writeLines(txt, tempfile)
+	knit2html(tempfile, ouptput)
+	file.remove(tempfile)
+}
