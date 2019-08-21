fa7e56b2cd5dd8423ed9b442b98a531262d4c01f
Zuguang Gu <jokergoo@gmail.com>
2019-08-21 22:25:59 +0200

    adjust the template

--- R/z.global.R
+++ R/z.global.R
@@ -19,7 +19,8 @@ names(brewer_pal_set2_col) = 1:16
 # == details
 # There are following global parameters:
 #
-# -``group_diff`` Used in `get_signatures,ConsensusPartition,method`.
+# -``group_diff`` Used in `get_signatures,ConsensusPartition-method`.
+# -``fdr_cutoff`` Used in `get_signatures,ConsensusPartition-method`.
 #
 cola_opt = function(..., RESET = FALSE, READ.ONLY = NULL, LOCAL = FALSE, ADD = FALSE) {}
 cola_opt = setGlobalOptions(
